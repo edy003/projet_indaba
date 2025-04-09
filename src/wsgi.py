6c6861cbbbc10268f2faa227faa53399ce2ed6e2
edy003 <97347567+edy003@users.noreply.github.com>
2025-04-09 10:11:31 +0100

    Delete src directory

--- src/wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Ajoutez le répertoire src au chemin Python
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-from indaba import server
-application = server
